17635927028230df0da0f0af16f49822d576b364
Amirhossein-Ra <000amir111rahzany@gmail.com>
2025-10-10 16:39:19 +0330

    Added login and register

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { FaSearch, FaUser } from "react-icons/fa";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import Badge from "@mui/material/Badge";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between h-[100px] shadow-md px-6 ">
-      <div className="cursor-pointer m-2 ">
-        <img src="/blisslogo1.png" alt="logo" height="200px" width="200px" />
-      </div>
+      <Link to="/">
+        <div className="cursor-pointer m-2 ">
+          <img src="/blisslogo1.png" alt="logo" height="200px" width="200px" />
+        </div>
+      </Link>
       <div className="flex items-center m-2">
         <input
           className="p-[15px] border-2 border-[#f096dd] border-solid w-[500px] outline-none rounded-lg mr-[-30px]"
@@ -18,17 +21,21 @@ export default function Navbar() {
       </div>
 
       <div className="flex items-center">
-        <div className="flex items-center mr-[20px]">
-          <Badge badgeContent={2} color="secondary">
-            <ShoppingBasketIcon className="text-pink-500 cursor-pointer" />
-          </Badge>
-        </div>
-        <div className="flex items-center space-x-2 border border-pink-300 p-2 rounded-lg hover:bg-pink-100 cursor-pointer">
-          <FaUser className="text-[#e455c5] hover:text-pink-600 transition duration-300" />
-          <span className="text-[#e455c5] hover:text-pink-600 font-semibold">
-            Login
-          </span>
-        </div>
+        <Link to="/cart">
+          <div className="flex items-center mr-[20px]">
+            <Badge badgeContent={2} color="secondary">
+              <ShoppingBasketIcon className="text-pink-500 cursor-pointer" />
+            </Badge>
+          </div>
+        </Link>
+        <Link to="/login">
+          <div className="flex items-center space-x-2 border border-pink-300 p-2 rounded-lg hover:bg-pink-100 cursor-pointer">
+            <FaUser className="text-[#e455c5] hover:text-pink-600 transition duration-300" />
+            <span className="text-[#e455c5] hover:text-pink-600 font-semibold">
+              Login
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Register() {
+  return (
+    <div className="flex items-center justify-center mt-[15%]">
+      <div className="flex items-center bg-white shadow-lg rounded-lg overflow-hidden">
+        {/* IMAGE */}
+        <div className="h-[500px] w-[500px] transition-transform duration-700 ease-in-out transform hover:scale-105">
+          <img
+            src="/lotion.jpg"
+            alt="login"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* FORM */}
+
+        <div className="p-10 w-[500px] h-[500px]">
+          <h2 className="text-xl font-semibold text-gray-700 mb-5">
+            Create Account
+          </h2>
+
+          <form className="space-y-5">
+            <div className="mb-5">
+              <label htmlFor="" className="block text-gray-600 mb-1">
+                Full Name
+              </label>
+              <input
+                type="text"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#d55fbb]"
+                placeholder="AmirHossein Rahzany"
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="" className="block text-gray-600 mb-1">
+                Email
+              </label>
+              <input
+                type="text"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#d55fbb]"
+                placeholder="example@example.com"
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="" className="block text-gray-600 mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#d55fbb]"
+                placeholder="*******"
+              />
+            </div>
+            <button className="w-full py-2 bg-[#d55fbb] font-black rounded-md transition-transform duration-500 hover:bg-blue-200 focus:outline-none focus:ring-red-500 hover:scale-105 text-white">
+              Create An Account
+            </button>
+
+            <div className="mt-4 text-sm text-gray-600">
+              <span className="">Have an account already?</span>
+              <Link className="ml-2 text-red-500 hover:underline" to="/login">
+                Login
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
